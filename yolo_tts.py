a31d8cac158869e5271ddd9da38ca0d2cdc9ad73
Prim9000 <prim9000@gmail.com>
2022-03-12 09:34:58 +0700

    Create yolo_tts.py

---
+++ yolo_tts.py
@@ -0,0 +1,39 @@
+import torch
+from gtts import gTTS
+from io import BytesIO
+import cv2
+
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Model
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+    # Inference
+    results = model(frame)
+
+    df = results.pandas().xyxy[0]
+
+    labels = str()
+
+    for label in df['name']:
+      labels += label + " "
+
+    myOutput = gTTS(text=labels, lang='en')
+    myOutput.save('talk.mp3')
+    os.system('mpg123 talk.mp3')
+ cv2.destroyAllWindows()
